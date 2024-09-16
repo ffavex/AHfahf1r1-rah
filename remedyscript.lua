0bf8ae6932cb243f7f9c6d69cdbf516da72dc31b
vzexi <vastcreation@protonmail.com>
2024-09-16 00:44:42 -0400

    Update remedyscript.lua

--- remedyscript.lua
+++ remedyscript.lua
@@ -9,7 +9,7 @@ local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
 local Window = Library:CreateWindow({
-    Title = 'Example Menu',
+    Title = 'Remedy.ez Private',
     Center = true,
     AutoShow = true,
     TabPadding = 8,
